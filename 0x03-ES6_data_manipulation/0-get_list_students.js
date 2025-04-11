dcf168f8460283a31a84fa16fdbcbb487ec1f299
Nsikak-mbre <tm260715@gmail.com>
2025-04-11 14:47:03 +0100

    array-manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+const getListStudents = () => {
+    return [
+        { id: 1, firstName: 'Gulliame', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+}
+
+export default getListStudents;
