f710355d284ed07cee3ab60a1b3051b8431ebb91
Nsikak-mbre <tm260715@gmail.com>
2025-04-13 17:27:33 +0100

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,8 @@
-const getListStudentIds = (arr) => {
-  if (Array.isArray(arr) === false) {
-    return [];
-  }
-  const studentIds = arr.map((student) => student.id);
-  return studentIds;
+export const getListStudentIds = (arr) => {
+    if (Array.isArray(arr) === false) {
+        return [];
+    }
+    const studentIds = arr.map((student) => student.id);
+    return studentIds;
 };
 
-export default getListStudentIds;
